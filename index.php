ce55a0aa73a1bb81038fc5037988caf48abbb8c8
zjhxmjl <zjhxmjl@gmail.com>
2013-11-12 15:01:51 +0800

    修复php5.4支持问题

--- index.php
+++ index.php
@@ -3,7 +3,10 @@
 /*the hdwiki entrance */
 error_reporting(0);
 //error_reporting(E_ALL);
-set_magic_quotes_runtime(0);
+
+@set_magic_quotes_runtime(false);
+ini_set('magic_quotes_runtime', 0);
+
 $mtime = explode(' ', microtime());
 $starttime = $mtime[1] + $mtime[0];
 

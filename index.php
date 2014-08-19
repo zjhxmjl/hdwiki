b132cd167d7b83ed5aa7ad517b1719260aea5ad1
zjhxmjl <zjhxmjl@localhost>
2014-08-19 11:11:00 +0800

    first commit

--- index.php
+++ index.php
@@ -3,21 +3,15 @@
 /*the hdwiki entrance */
 error_reporting(0);
 //error_reporting(E_ALL);
-
-// @set_magic_quotes_runtime(false);从php5.3起已经弃用
-if (version_compare(PHP_VERSION, '5.3.0', '<')) {
-	@set_magic_quotes_runtime(false);
-}
- ini_set('magic_quotes_runtime', 0);//关闭magic_quotes_runtime
-
+set_magic_quotes_runtime(0);
 $mtime = explode(' ', microtime());
 $starttime = $mtime[1] + $mtime[0];
-	
+
 define('IN_HDWIKI', TRUE);
-define('HDWIKI_ROOT', dirname(__FILE__));//返回路径的父目录
+define('HDWIKI_ROOT', dirname(__FILE__));
 
 include HDWIKI_ROOT.'/model/hdwiki.class.php';
 
-$hdwiki = new hdwiki();//新建一个类的实例
+$hdwiki = new hdwiki();
 $hdwiki->run();
 ?>

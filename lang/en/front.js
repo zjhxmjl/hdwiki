c803d196731023f6c9a6cb0f8fc22eb35bbcebfd
zjhxmjl <zjhxmjl@localhost>
2015-04-13 19:07:57 +0800

    add EN language packages

---
+++ lang/en/front.js
@@ -0,0 +1,22 @@
+var Lang =
+{
+Login : "Login",
+sendMessage: "Send Message",
+Loading : "Loading...",
+Subject : "Subject",
+Content : "Content",
+Submit : "Submit",
+Submiting : "Submiting...",
+To : "To",
+
+//tip info
+TipUserName : "Username length must between 3-15 characters!",
+TipUserNotExist : "Username does not exist!",
+TipPassword : "Password length: 6-12 characters!",
+TipCode : "Verification code error!",
+TipMessageLength : "300 characters at most.",
+TipMessageSendOk : "Send message OK.",
+TipMessageSendError : "Send message error.",
+TipMessageSubjectIsNull : 'Please input the message subject.',
+TipMessageContentIsNull : 'Please input the message content.'
+}
